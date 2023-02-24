4681b5f51333f732ff1736f16f82d3b496f6354e
Elena Kulichkova <ekulichk@1-D-8.42heilbronn.de>
2023-02-24 18:28:12 +0100

    refactor: read_map.c

--- private.h
+++ private.h
@@ -6,7 +6,7 @@
 /*   By: ekulichk <ekulichk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/18 19:35:58 by ekulichk          #+#    #+#             */
-/*   Updated: 2023/02/23 03:15:24 by ekulichk         ###   ########.fr       */
+/*   Updated: 2023/02/24 18:21:55 by ekulichk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,12 +18,30 @@
 # include "lib/MLX42/include/MLX42/MLX42.h"
 # include "lib/libft/libft.h"
 
+typedef enum e_map_component
+{
+	SPACE = 0,
+	WALL = 1,
+	COLLECTIBLE = 'C',
+	EXIT = 'E',
+	PLAYER = 'P',
+}	t_map_component;
+
 typedef struct s_params
 {
-	char	**map;
+	// t_map_component	**map;
+	char			**map;
+	size_t			map_capacity;
+	size_t			count;
+	int				len_line;
+	int				flag;
 }	t_params;
 
 // read_map.c
 void	read_map(int fd, t_params *params);
+void	map_add_line(t_params *params, char *line);
+void	map_extend(t_params *params);
+void	map_init(t_params *self);
+void	print_map(t_params *params);
 
 #endif
--- read_map.c
+++ read_map.c
@@ -6,7 +6,7 @@
 /*   By: ekulichk <ekulichk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/23 02:50:23 by ekulichk          #+#    #+#             */
-/*   Updated: 2023/02/23 03:19:48 by ekulichk         ###   ########.fr       */
+/*   Updated: 2023/02/24 18:26:39 by ekulichk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,57 @@
 
 void	read_map(int fd, t_params *params)
 {
-	int		len;
 	char	*line;
 
-	*params->map = get_next_line(fd);
-	len = ft_strlen(line);
-	printf("%s\n", *params->map);
+	map_init(params);
+	line = get_next_line(fd);
+	params->len_line = ft_strlen(line);
+	params->flag = 0;
+	while (params->flag == 0)
+	{
+		line = get_next_line(fd);
+		if (ft_strlen(line) != params->len_line)
+		{
+			printf("Wrong map\n");
+			return (0);
+		}
+		else if (line == NULL)
+			params->flag = 1;
+	}
+	map_add_line(params, line);
+	print_map(params);
+}
+
+void	map_add_line(t_params *params, char *line)
+{
+	if (params->map_capacity == params->count)
+		map_extend(params);
+	params->map[params->count] = line;
+	params->count += 1;
+}
+
+void	map_extend(t_params *params)
+{
+	char	**new_map;
+	size_t	new_capacity;
+
+	new_capacity = params->map_capacity * 2;
+	new_map = malloc(sizeof(char *) * new_capacity);
+	ft_memcpy(new_map, params->map, params->map_capacity);
+	params->map = new_map;
+	params->map_capacity = new_capacity;
+}
+
+void	map_init(t_params *params)
+{
+	params->map_capacity = 1;
+	params->count = 0;
+	params->map = malloc(sizeof(char *) * params->map_capacity);
+}
+
+void	print_map(t_params *params)
+{
+	printf("%s", params->map[0]);
+	// printf("%s", params->map[1]);
+	// printf("%s", params->map[2]);
 }
